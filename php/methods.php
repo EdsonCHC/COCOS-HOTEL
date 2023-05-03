b37c399de9c0e90531900965a1aa3f4f5c7c744b
Edson CHC <105886687+EdsonCHC@users.noreply.github.com>
2023-05-03 07:52:57 -0600

    Actualizar methods.php
    Se agrega campo time

--- php/methods.php
+++ php/methods.php
@@ -7,8 +7,8 @@ class methods
 
         $conection = $c->conexion();
 
-        $sql = "INSERT INTO reservas (name, lastname, phone, room, date, code)
-                    VALUES ('$datos[0]','$datos[1]', '$datos[2]', '$datos[3]', '$datos[4]', '$datos[5]')";
+        $sql = "INSERT INTO reservas (name, lastname, phone, room, date, time, code)
+                    VALUES ('$datos[0]','$datos[1]', '$datos[2]', '$datos[3]', '$datos[4]', '$datos[5]', '$datos[6]')";
 
         return $result = mysqli_query($conection,$sql);
     }
